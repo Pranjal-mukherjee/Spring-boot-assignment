f6472a1d96289c1152e02be3a552e9e68934db64
Pranjal <pranjal.mukharjee@zemosolabs.com>
2023-07-04 17:45:23 +0530

    Coverage

---
+++ src/main/java/com/zemoso/springboot/assignment/controller/BookController.java
@@ -0,0 +1,63 @@
+package com.zemoso.springboot.assignment.controller;
+
+import com.zemoso.springboot.assignment.DTO.BookDTO;
+import com.zemoso.springboot.assignment.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+
+    private BookService bookService;
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    // add mapping for GET /Books
+    @GetMapping("/")
+    public ResponseEntity<List<BookDTO>> getAllBook() {
+        List<BookDTO> bookDTOS = bookService.getAllBooks();
+        return ResponseEntity.ok(bookDTOS);
+        //here we can also write the below line if we do not want security
+        //return BookService.getAllBooks();
+//        The ResponseEntity.ok() method is used to create an HTTP response with a status code of 200 (OK)
+//        and a response body containing the specified data.
+//                It is commonly used in web applications to return a successful response along with
+//                the desired data.
+    }
+
+    // add mapping for GET /Books/{BookId}
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
+        BookDTO bookDTO = bookService.getBookById(id);
+        return ResponseEntity.ok(bookDTO);
+    }
+
+    // add mapping for POST /Books - add new Book
+    @PostMapping("/")
+    public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO bookDTO) {
+        System.out.println(bookDTO.toString());
+        BookDTO createBookDTo = bookService.createBook(bookDTO);
+        return ResponseEntity.ok(createBookDTo);
+    }
+
+    // add mapping for UPDATE /Books/{BookId} - update existing Books
+    @PutMapping("/")
+    public ResponseEntity<BookDTO> updateBook(@RequestBody BookDTO bookDTO) {
+        BookDTO updatedBookDTO = bookService.updateBook(bookDTO);
+        return ResponseEntity.ok(updatedBookDTO);
+    }
+
+    // add mapping for DELETE /Books/{BookId} - delete Books by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
